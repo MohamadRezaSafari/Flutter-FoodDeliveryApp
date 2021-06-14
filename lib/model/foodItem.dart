a7f19b64d36ca062a7cb00b783abde10d4fcb27e
Reza Safari <ali272897@gmail.com>
2021-06-14 22:10:50 +0430

    img links

--- lib/model/foodItem.dart
+++ lib/model/foodItem.dart
@@ -7,7 +7,7 @@ FoodItemList fooditemList = FoodItemList(foodItems: [
     hotel: "Las Vegas Hotel",
     price: 14.49,
     imgUrl:
-        "https://hips.hearstapps.com/pop.h-cdn.co/assets/cm/15/05/480x240/54ca71fb94ad3_-_5summer_skills_burger_470_0808-de.jpg",
+        "https://b.zmtcdn.com/data/pictures/chains/8/18427868/1269c190ab2f272599f8f08bc152974b.png",
   ),
   FoodItem(
     id: 2,
@@ -22,21 +22,24 @@ FoodItemList fooditemList = FoodItemList(foodItems: [
     title: "Supreme Pizza Burger",
     hotel: "Golf Course",
     price: 8.49,
-    imgUrl: "https://static.food2fork.com/burger53be.jpg",
+    imgUrl:
+        "https://b.zmtcdn.com/data/pictures/chains/8/18427868/1269c190ab2f272599f8f08bc152974b.png",
   ),
   FoodItem(
     id: 4,
     title: "Chilly Cheeze Burger",
     hotel: "Las Vegas Hotel",
     price: 14.49,
-    imgUrl: "https://static.food2fork.com/36725137eb.jpg",
+    imgUrl:
+        "https://b.zmtcdn.com/data/pictures/chains/8/18427868/1269c190ab2f272599f8f08bc152974b.png",
   ),
   FoodItem(
     id: 5,
     title: "Beach BBQ Burger",
     hotel: "Las Vegas Hotel",
     price: 14.49,
-    imgUrl: "https://static.food2fork.com/turkeyburger300x200ff84052e.jpg",
+    imgUrl:
+        "https://b.zmtcdn.com/data/pictures/chains/8/18427868/1269c190ab2f272599f8f08bc152974b.png",
   ),
   FoodItem(
     id: 6,
@@ -44,7 +47,7 @@ FoodItemList fooditemList = FoodItemList(foodItems: [
     hotel: "Las Vegas Hotel",
     price: 14.49,
     imgUrl:
-        "https://cdn.pixabay.com/photo/2018/03/04/20/08/burger-3199088__340.jpg",
+        "https://b.zmtcdn.com/data/pictures/chains/8/18427868/1269c190ab2f272599f8f08bc152974b.png",
   ),
 ]);
 
